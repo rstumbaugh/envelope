1b0ed53f48d9afab5d7f2f758036253e5c0b445f
Ryan Stumbaugh <rstumbaugh1@gmail.com>
2023-10-03 11:27:59 -0400

    initial commit, boilerplate

---
+++ Envelope.Services/Envelope.Budget.Service/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Envelope.Budget.Service.Controllers
+{
+    public enum Test
+    {
+        Value
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Hello()
+        {
+            return Json(new
+            {
+                Response = "Hello",
+                test = new Dictionary<string, string>
+                {
+                    ["Test"] = "value"
+                },
+                testEnum = Test.Value
+            });
+        }
+    }
+}
